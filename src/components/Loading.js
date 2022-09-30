b7270deba2249c8de7b65d73c7aefd61dd25ff86
IreneLee18 <cc86418520@gmail.com>
2022-09-30 22:58:46 +0800

    add loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,21 @@
+import PacmanLoader from "react-spinners/PacmanLoader";
+import { useState } from "react";
+function Loading() {
+  const css = {
+    margin: "20px",
+  };
+  const [size, setSize] = useState(80);
+  // 調整loading大小
+  window.addEventListener("resize", () => {
+    if (window.screen.width <= 375) {
+      setSize(() => 30);
+    } else if (window.screen.width <= 768 && window.screen.width > 375) {
+      setSize(() => 50);
+    } else {
+      setSize(() => 80);
+    }
+  });
+  return <PacmanLoader color="#533f34" size={size} cssOverride={css} />;
+}
+
+export default Loading;
