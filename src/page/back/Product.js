e4240cbac9c916eed0ce08b204f66a3823569654
IreneLee18 <cc86418520@gmail.com>
2022-10-02 23:32:56 +0800

    add prosuctmodal update scss-rwd

--- src/page/back/Product.js
+++ src/page/back/Product.js
@@ -1,9 +1,67 @@
+// import TableFrom from '../../components/TableFrom'
+import ProductModal from "../../components/Modal/ProductModal";
+import { useRef } from "react";
 function Product() {
+  const productModalRef = useRef();
+  const handleClickOpenModal = () => {
+    productModalRef.current.openModal();
+  };
   return (
     <>
       <div className="container">
-        <h1>jjj</h1>
+        <section>
+          <div className="search">
+            <select name="" id="">
+              <option value="類別">類別</option>
+              <option value="名稱">名稱</option>
+              <option value="啟用">啟用</option>
+            </select>
+            <label htmlFor="search">
+              <input type="text" id="search" placeholder="SEARCH" />
+              <button className="material-symbols-outlined">search</button>
+            </label>
+          </div>
+        </section>
+        <section>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>類別</th>
+                <th>名稱</th>
+                <th>原價</th>
+                <th>特價</th>
+                <th>編輯</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>養肌系列</td>
+                <td>藻針</td>
+                <td>3000</td>
+                <td>3000</td>
+                <td>
+                  <button className="material-symbols-outlined">edit</button>
+                  <button className="material-symbols-outlined">delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <div>
+          <button
+            className="dashbord-add material-symbols-outlined"
+            onClick={handleClickOpenModal}
+          >
+            add_circle
+          </button>
+          <button className="scroll-top material-symbols-outlined">
+            assistant_navigation
+          </button>
+        </div>
       </div>
+      <section>
+        <ProductModal ref={productModalRef} />
+      </section>
     </>
   );
 }
--- src/page/front/Home/Home.js
+++ src/page/front/Home/Home.js
@@ -5,11 +5,7 @@ function Home() {
       <section>
         <Slider />
       </section>
-      <section>
-        <div className="slogan"></div>
-        <p>不是低價優勢，而是品質保證</p>
-        <p>每一步的選擇，只為給您最好的</p>
-      </section>
+      <section></section>
     </>
   );
 }
